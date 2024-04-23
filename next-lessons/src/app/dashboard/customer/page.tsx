e9b8c06cf2e39c85b6ad87c394c1b778f4bb09a9
patrick <patrickwuke@163.com>
2024-04-23 18:53:23 +0800

    #Step9:Create Customer Pages

--- next-lessons/src/app/dashboard/customer/page.tsx
+++ next-lessons/src/app/dashboard/customer/page.tsx
@@ -1,9 +1,26 @@
+import { Metadata } from 'next';
+import {fetchCustomers} from "@/lib/dashboard/store";
+import CustomersTable from "@/app/ui/components/customers/table";
 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
-export default function Page(){
-    return (
-        <div>
-            <p>Customer Page</p>
-        </div>
-    )
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }

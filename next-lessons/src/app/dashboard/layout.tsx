452db2734cac625fbf90ac8553994c243afb1569
patrick <patrickwuke@163.com>
2024-04-23 14:44:47 +0800

    #Step5: Refine Layout

--- next-lessons/src/app/dashboard/layout.tsx
+++ next-lessons/src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import SideNav from "@/app/ui/components/dashboard/sidenav";
 
 
 export default function Layout({
@@ -5,8 +6,12 @@ export default function Layout({
                                }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return (<div>
-            {children}
+    return (
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+                <SideNav/>
+            </div>
+            <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
     );
 }

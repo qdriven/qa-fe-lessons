8bdf605491c7a09cd35c7222840dbdc4559ae455
patrick <patrickwuke@163.com>
2024-04-23 18:30:19 +0800

    #Step6: Complete Dashboard Page

--- next-lessons/src/app/dashboard/(overview)/page.tsx
+++ next-lessons/src/app/dashboard/(overview)/page.tsx
@@ -1,17 +1,30 @@
-import Link from "next/link";
-import {ArrowRightIcon} from "@heroicons/react/24/outline";
-import SideNav from "@/app/ui/components/dashboard/sidenav";
+import {inter} from "@/app/ui/fonts";
+import {Suspense} from "react";
+import {CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/components/skeletons";
+import CardWrapper from "@/app/ui/components/dashboard/cards";
+import LatestInvoices from "@/app/ui/components/dashboard/latest-invoice";
+import RevenueChart from "@/app/ui/components/dashboard/revenue";
 
 
-export default function Page(){
+export default function Page() {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-                <SideNav/>
+        <main>
+            <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton/>}>
+                    <CardWrapper/>
+                </Suspense>
             </div>
-            <section>
-                <p>Main Section</p>
-            </section>
-        </div>
+            <div className="mt-6 grid gap-6 grid-cols md:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<RevenueChartSkeleton/>}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
     )
 }
--- next-lessons/src/app/dashboard/invoice/page.tsx
+++ next-lessons/src/app/dashboard/invoice/page.tsx
@@ -1,9 +1,40 @@
+import {Metadata} from "next";
+import {inter} from "@/app/ui/fonts";
+import {Suspense} from "react";
 
 
-export default function Page(){
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
+
+export default async function Page({
+                                       searchParams,
+                                   }: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const totalPages = 10;
+
     return (
-        <div>
-            <p>Invoice Page</p>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${inter.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                {/*<Search placeholder="Search invoices..." />*/}
+                {/*<CreateInvoice />*/}
+            </div>
+            {/*<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>*/}
+            {/*    <Table query={query} currentPage={currentPage} />*/}
+            {/*</Suspense>*/}
+            {/*<div className="mt-5 flex w-full justify-center">*/}
+            {/*    <Pagination totalPages={totalPages} />*/}
+            {/*</div>*/}
         </div>
-    )
+    );
 }
